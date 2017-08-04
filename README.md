59043be4e47f7b35f9cba3a24b228be743a091f6
vperezb-lenovo <victoor5@gmail.com>
2017-08-04 22:52:46 +0200

    Changes in functions and live example

--- README.md
+++ README.md
@@ -1,10 +1,45 @@
-# [developing] google-api-python-tools
+# google-api-python-tools
+
 Some functions to make Google APIs more accesible using Pandas. 
+
+## Steps
+
+1. Create google service account 
+    1. Go to https://console.developers.google.com/projectselector/apis/credentials
+    2. Create new project
+    3. Share the file you want to access with the service "account" :
+        first-service-account@example-id-175820.iam.gserviceaccount.com
+
 
 ## Motivation
 I've been working with some Google APIs and I would like to share what I've learned.
+
+[READ _RAW_ DATA] - [WORK WITH DATA] - [CREATE VALUABLE DATA] - [DISPLAY DATA]
+
+I wanted to use Google APIs to develop a dashboard to analize and display data from Google Analytics, Google SearchConsole and Google Drive.
+
+After retrieve the data, we can modify, filter, compare, delete and work with it 
 
 ## APIs used
 * Sheets
 * Analytics
 * SearchConsole
+* ... Someday
+
+
+## Auth
+There are two ways of authenticating to be able to make Google API calls.
+* By redirecting the user to the web browser and then Log-in (the typical) within the account you want to use de data.
+* By creating a Google service and download its credentials.json.
+
+### When use client auth
+If you are developing tools to deploy it in a computer with graphical env.
+Is the easier to start and have full access to the info.
+
+### When use Google service account
+* If you want your API (or someones external API) to have access to only the services you want. IE: You built an API to read each morning your boss "todo today" document. In order to read it he can add _readonly_ permisions to the desired file to your service account. 
+
+## Service accounts
+
+#### Furhter information
+
--- first_functions.py
+++ first_functions.py
@@ -9,19 +9,24 @@ def get_configs(CONFIG_PATH):
     parserconfig = configparser.ConfigParser()
     parserconfig.read(os.path.join("./" , CONFIG_PATH))
     return parserconfig
-	
-def start_service(Config, SERVICE_NAME):
+
+# Start service using a GoogleService
+
+def start_service(Config, SERVICE_ID):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         Config.get("Credentials", 'secretsPath'), 
-        scopes='https://www.googleapis.com/auth/' + Config.get("Services", SERVICE_NAME)
+        scopes='https://www.googleapis.com/auth/' + Config.get(SERVICE_ID, 'url_path')
     )
     service = build(
-        SERVICE_NAME,
-        Config.get("Services", "v_" + SERVICE_NAME),
+        Config.get(SERVICE_ID, "build"),
+        Config.get(SERVICE_ID, "version"),
         http=credentials.authorize(Http()),
         cache_discovery=False
     )
     return service
+
+# Start service with web browser authentication
+
 
 def change_sheet_title(service, newFileName, fileId):
     
@@ -41,7 +46,18 @@ def change_sheet_title(service, newFileName, fileId):
     
     return
 
-def pandas_to_sheet(service, sheetId, pageName, columnsList ,valuesList):
+def pandas_to_sheet(service, sheetId, pageName, df):
+    
+    '''
+    Uploads a pandas.dataframe to the desired page of a google sheets sheet.
+    SERVICE ACCOUNT MUST HAVE PERMISIONS TO WRITE IN THE SHEET.
+    Aditionally, pass a list with the new names of the columns.    
+    Data must be utf-8 encoded to avoid errors.
+    '''
+    
+    df.fillna(value = 0, inplace=True)
+    columnsList = df.columns.tolist()
+    valuesList = df.values.tolist()
 
     try:
         data = [
@@ -64,13 +80,22 @@ def pandas_to_sheet(service, sheetId, pageName, columnsList ,valuesList):
         return 'True' 
     
     except e as Exception:
-        return e
+        print (e)
 
 def get_sheet_names(sheets_service,sheetId):
     response = sheets_service.spreadsheets().get(spreadsheetId=sheetId).execute()
     return [ a['properties']['title'] for a in response['sheets']]
 
 def sheet_to_pandas(service, spreadsheetId ,sheetName='',sheetRange='',index=''):
+    '''
+    PARAMETERS:
+        service - Api service
+        spreadsheetId - Id of the desired document
+        sheetName - Name of the desired page 'Hoja1' (optional) (by default: first page)
+        sheetRange - Range of the desired info 'A1:C6' (optional) (by default: WHOLE PAGE)
+        index - column you want to be the index of the resulting dataframe (optional) (by default: none of the columns is set as index)
+    '''
+    
     if (sheetRange != ''): sheetRange='!'+sheetRange
         
     newresult = service.spreadsheets().values().get(
@@ -85,11 +110,3 @@ def sheet_to_pandas(service, spreadsheetId ,sheetName='',sheetRange='',index='')
         return pd.DataFrame(newresult['values'],columns = headers)
     else:
         return pd.DataFrame(newresult['values'],columns = headers).set_index(index, drop=False)
-    
-    #PARAMETERS:
-        #service - Api service
-        #spreadsheetId - Id of the desired document
-        #sheetName - Name of the desired page 'Hoja1' (optional) (by default: first page)
-        #sheetRange - Range of the desired info 'A1:C6' (optional) (by default: WHOLE PAGE)
-        #index - column you want to be the index of the resulting dataframe (optional) (by default: none of the columns is set as index)
-    
